7b08966534702ded61432ac582904826d20c8137
AlexandrBxbrxv <g1026@bk.ru>
2025-02-16 06:50:51 +0500

    + сериализаторы для моделей Contact, Network, Product

---
+++ electronics/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+
+from electronics.models import Contact, Network, Product
+
+
+class ContactSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Contact."""
+
+    class Meta:
+        model = Contact
+        fields = "__all__"
+        read_only_fields = ("owner",)
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Product."""
+
+    class Meta:
+        model = Product
+        fields = "__all__"
+        read_only_fields = ("owner",)
+
+
+class NetworkSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Network."""
+
+    products = ProductSerializer(source='product_network', many=True)
+    level = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Network
+        fields = "__all__"
+        read_only_fields = ("owner",)
+
+    @staticmethod
+    def get_level(network) -> int:
+        """Возвращает цифру уровня на котором находится сеть."""
+        if network.supplier is None:
+            return 0
+        if network.supplier:
+            if network.supplier.supplier:
+                return 2
+            return 1
